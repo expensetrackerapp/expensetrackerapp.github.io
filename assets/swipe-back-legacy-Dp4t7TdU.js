0350dff7cbc83d4a46a429a76ff7cce26d8545e0
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-06-22 16:56:19 +0530

    Update ui

--- assets/swipe-back-legacy-Dp4t7TdU.js
+++ assets/swipe-back-legacy-Dp4t7TdU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C72uPSMv.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Cuyc6fvK.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
