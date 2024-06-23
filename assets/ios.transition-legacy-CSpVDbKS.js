8568b2222f2910fbbb2766daab6c3e62e6d6c7eb
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-06-23 17:20:26 +0530

    update ui

--- assets/ios.transition-legacy-CSpVDbKS.js
+++ assets/ios.transition-legacy-CSpVDbKS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cuyc6fvK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BVkxXxWy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
