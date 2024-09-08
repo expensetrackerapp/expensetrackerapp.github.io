41cb0335748ed054c53efafcabfcb40716d0556b
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-09-08 15:59:24 +0530

    Fix graph update

--- assets/swipe-back-legacy-DGqEJ7YX.js
+++ assets/swipe-back-legacy-DGqEJ7YX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFrNoMMf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Dg9LwWFa.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
