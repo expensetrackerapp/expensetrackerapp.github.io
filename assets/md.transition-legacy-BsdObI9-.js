e05ffd134a23669d63c186606cd3a58e68c20ca7
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-09-28 15:05:57 +0530

    Add export feature

--- assets/md.transition-legacy-BsdObI9-.js
+++ assets/md.transition-legacy-BsdObI9-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dg9LwWFa.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-w46WMeE3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
